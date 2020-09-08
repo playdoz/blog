de2cbf27d2cfdc8b82eac930ac1cf6f1b361e019
kgov <kim.gov@latitudefinancial.com>
2020-09-09 00:43:18 +1000

    New moderation service

--- query/index.js
+++ query/index.js
@@ -14,6 +14,8 @@ app.get('/posts', (req, res) => {
 
 app.post('/events', (req, res) => {
   const { type, data } = req.body;
+  console.log('Received event:', type);
+
 
   if (type === 'PostCreated') {
     const { id, title } = data;
@@ -22,13 +24,24 @@ app.post('/events', (req, res) => {
   }
 
   if (type === 'CommentCreated') {
-    const { id, content, postId } = data;
+    const { id, content, postId, status } = data;
 
     const post = posts[postId];
-    post.comments.push({ id, content });
+    post.comments.push({ id, content, status });
   }
 
-  console.log(posts);
+  if (type === 'CommentUpdated') {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    const comment = post.comments.find(comment => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+    comment.content = content;
+  }
+
   res.send({});
 });
 
